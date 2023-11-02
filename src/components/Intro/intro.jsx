023ae32679a29b2b053a88cc0d131e77337d1e16
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-02 12:09:24 +0330

    intro update

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './intro.css';
+import hireImg from '../../assets/hireme.png';
+import bg from '../../assets/image.png';
+import { Link } from 'react-scroll';
 
 const Intro = () => {
   return (
-    <div>intro</div>
+    <section id="intro">
+      <div className="left-intro">
+        <span className="hello">Hello,</span>
+        <span className='introName'>I'm <span className='introNameColor'>Amirmohammad</span><br />Website Designer</span>
+        <p className='introParagraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+        eleniti eos facere est incidunt beatae. Saepe sit architecto dolores magnam suscipit!</p>
+        <Link><button className="introHireMe"><img src={hireImg} alt="hire" className="introBtnImg" />Hire Me</button></Link>
+      </div>
+      <img src={bg} alt="bachground" className="bg" />
+    </section>
   )
 }
 
