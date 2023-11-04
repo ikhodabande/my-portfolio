1665849d7d68facc20ed5288133241653c7e6a39
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-04 20:51:45 +0330

    nav update for mobile

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contact.png';
 import { Link } from 'react-scroll';
-
+import menu from '../../assets/menu.png';
 
 const  Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return ( 
     <nav className='navbar'>
       <img src={logo} alt='logo' className='logo'></img>
@@ -18,6 +19,17 @@ const  Navbar = () => {
       <button className="desktopMenuBtn" onClick={()=>
         document.getElementById('contact').scrollIntoView({behavior:'smooth'})}>
         <img src={contactImg} alt="contact" className="desktopMenuImg" /> Contact me </button>
+   
+        <img src={menu} alt='Menu' className='moblogo' onClick={()=>setShowMenu(!showMenu)}></img>
+      <div className="navMenu" style={{display : showMenu ? 'flex' : 'none' }}>
+       <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem" onClick={()=>setShowMenu(false)}>Home</Link>
+       <Link activeClass='active' to='skill' spy={true} smooth={true} offset={-30} duration={500}  className="desktopMenuListItem" onClick={()=>setShowMenu(false)}>About</Link>
+       <Link activeClass='active' to='works' spy={true} smooth={true} offset={-30} duration={500}  className="desktopMenuListItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+       <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500}  className="desktopMenuListItem" onClick={()=>setShowMenu(false)}>Client</Link>
+       <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500}  className="desktopMenuListItem" onClick={()=>setShowMenu(false)}>Contact Me</Link>
+      </div>
+   
+   
     </nav>
    );
 }
