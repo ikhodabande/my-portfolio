9bfb62f94de7a0af6c640e40241aacbe9e26d8a7
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-07 09:42:09 +0330

    update

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './intro.css';
 import hireImg from '../../assets/hireme.png';
 import bg from '../../assets/image.png';
-import { Link } from 'react-scroll';
+
+
 
 const Intro = () => {
   return (
@@ -10,9 +11,9 @@ const Intro = () => {
       <div className="left-intro">
         <span className="hello">Hello ,</span>
         <span className='introName'>I'm <span className='introNameColor'>AmirMohammad</span><br />Website Designer</span>
-        <p className='introParagraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-        eleniti eos facere est incidunt beatae. Saepe sit architecto dolores magnam suscipit!</p>
-        <Link to='https://www.linkedin.com/in/amikhodabande' target='_blank'><button className="introHireMe"><img src={hireImg} alt="hire" className="introBtnImg" />Hire Me</button></Link>
+        <p className='introParagraph'>As a hard-working newbie in the field of web development ,<br />
+         I possess a strong foundation in HTML, CSS, and JavaScript , Now I'm on track to develop my skills in React</p>
+        <a href='https://www.linkedin.com/in/amikhodabande' target='_blank'><button className="introHireMe"><img src={hireImg} alt="hire" className="introBtnImg" />Hire Me</button></a>
       </div>
       <img src={bg} alt="bachground" className="bg" />
     </section>
--- src/components/Skill/skill.jsx
+++ src/components/Skill/skill.jsx
@@ -9,27 +9,33 @@ function Skill() {
   return (
     <section id="skill">
         <span className="headerSkillTitle">What I Do</span>
-        <span className="headerSkillParagraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, sit quisquam nesciunt modi quo molestias laudantium corrupti. Laboriosam, eveniet voluptates.</span>  
+        <span className="headerSkillParagraph">As a web designer, I'm passionate about creating visually stunning and user-friendly websites. I believe that
+          design plays a crucial role in making a strong online presence, and my goal is to craft websites that not only
+          look beautiful but also provide a seamless user experience.</span>  
       <div className="skillBars">
         <div className="skillBar">
           <img src={uiImg} alt="" className="skillBarImg" />
           <div className="skillBarDes">
-            <h1>Ui/Ux Designer</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae ratione alias ab, vero corrupti quibusdam placeat voluptates consequuntur et.</p>
+            <h1>Front-end Developer</h1>
+            <p>I specialize in creating web applications with React and front-end development technologies. My goal is to build
+          responsive, user-friendly, and visually appealing websites.</p>
           </div>
         </div>
         <div className="skillBar">
           <img src={webImg} alt="" className="skillBarImg" />
           <div className="skillBarDes">
             <h1>Web Designer</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta perferendis, consequuntur eius blanditiis corrupti molestias vero unde iure voluptates.</p>
+            <p>As a web designer, I focus on creating visually appealing and user-friendly websites that make a great first impression.
+          I pay attention to every detail to ensure that the design reflects the uniqueness of each project.</p>
           </div>
         </div>
         <div className="skillBar">
           <img src={appImg} alt="" className="skillBarImg" />
           <div className="skillBarDes">
-            <h1>App Designer</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt saepe optio expedita consectetur. Perferendis beatae placeat recusandae, quasi pariatur repellat.</p>
+            <h1>UI/UX Designer</h1>
+            <p>As a UI/UX designer, I specialize in creating user-friendly interfaces and delightful user experiences. My design
+          approach focuses on both aesthetics and usability, ensuring that every project is visually appealing and easy to
+          navigate.</p>
           </div>
         </div>
       </div>
