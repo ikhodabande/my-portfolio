b9d6a807823fb108dfaa918aa92fec52f81a2c4a
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-07 09:20:40 +0330

    sync

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
         <span className='introName'>I'm <span className='introNameColor'>AmirMohammad</span><br />Website Designer</span>
         <p className='introParagraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
         eleniti eos facere est incidunt beatae. Saepe sit architecto dolores magnam suscipit!</p>
-        <Link to='#'><button className="introHireMe"><img src={hireImg} alt="hire" className="introBtnImg" />Hire Me</button></Link>
+        <Link to='https://www.linkedin.com/in/amikhodabande' target='_blank'><button className="introHireMe"><img src={hireImg} alt="hire" className="introBtnImg" />Hire Me</button></Link>
       </div>
       <img src={bg} alt="bachground" className="bg" />
     </section>
